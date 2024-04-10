647b5d0d754422c4a78a197b4c0c340524d76cbf
武帅祺 <1262458648@qq.com>
2024-04-10 11:34:51 +0800

    删除测试代码

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"context"
 	"file-service/common"
 	"file-service/routers"
-	"fmt"
-	"github.com/apache/rocketmq-client-go/v2"
-	"github.com/apache/rocketmq-client-go/v2/consumer"
-	"github.com/apache/rocketmq-client-go/v2/primitive"
 	"log"
-	"time"
 )
 
 func main() {
-
-	consumerTest()
+	webServiceStart()
 }
 
 func webServiceStart() {
@@ -40,31 +33,4 @@ func webServiceStart() {
 		return
 	}
 	log.Println("Gin Web Serve Start Success...")
-}
-
-func consumerTest() {
-	c, err := rocketmq.NewPushConsumer(
-		consumer.WithGroupName("test"),
-		consumer.WithNsResolver(primitive.NewPassthroughResolver([]string{"127.0.0.1:9876"})),
-	)
-	if err != nil {
-		panic(err)
-	}
-	if err := c.Subscribe("hello",
-		consumer.MessageSelector{},
-		// 收到消息后的回调函数
-		func(ctx context.Context, msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
-			for i := range msgs {
-				fmt.Printf("获取到值： %v \n", msgs[i])
-			}
-			return consumer.ConsumeSuccess, nil
-		}); err != nil {
-	}
-	err = c.Start()
-	if err != nil {
-		panic("启动consumer失败")
-	}
-	//不能让主goroutine退出
-	time.Sleep(time.Hour)
-	_ = c.Shutdown()
 }
